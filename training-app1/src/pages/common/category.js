8d2cba77453410fc65232480cc1c47f81c3ca99d
Gagana97 <cgagana97@gmail.com>
2023-10-04 14:34:57 +0530

    .

--- training-app1/src/pages/common/category.js
+++ training-app1/src/pages/common/category.js
@@ -1,127 +1,4 @@
-/* 
-import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
 
-
-
-
-function CategoryList() {
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [subcategories, setSubcategories] = useState([]);
-  const [videos, setVideos] = useState("");
-  const [selectedSubcategory, setSelectedSubcategory] = useState('');
-  const [selectedVideo, setSelectedVideo] = useState('');
-  const { categoryId, subcategoryId } = useParams();
-
-
-
-  
-  useEffect(() => {
-    async function fetchVideos() {
-      const response = await axios.get(`http://localhost:5000/subcategories/${subcategoryId}/videos`);
-      setVideos(response.data);
-      setCategories(response.data);
-
-    }
-    fetchVideos();
-  }, [categoryId, subcategoryId]);
-  useEffect(() => {
-    async function fetchCategories() {
-      const response = await axios.get('http://localhost:5000/categories');
-      setCategories(response.data);
-      setVideos(response.data);
-
-    }
-    fetchCategories();
-  }, []);
-
-
-  var onClick1 = (event) => {
-    const selectedCategory = event.target.value;
-
-    setSelectedCategory(selectedCategory);
-
-    const category = categories.find((c) => c.name === selectedCategory);
-    setSubcategories(category.subcategories);
-
-
-
-
-  };
-  const onClick = (event) => {
-    const selectedSubcategory = event.target.value;
-
-    setSelectedSubcategory(selectedSubcategory);
-
-    const subcategory = subcategories.find((c) => c.name === selectedSubcategory);
-    setVideos(subcategory.videos);
-
-
-
-
-  };
-
-
-
-
-
-  return (
-
-    <div>
-
-      <select value={selectedCategory} onChange={onClick1}>
-        <option value="">Categories</option>
-
-        {categories.map((category) => (
-          <option key={category.name} value={category.name}>
-            {category.name}
-          </option>
-
-        ))}
-
-      </select>
-
-      {subcategories.length > 0 && (
-        <div>
-          <select value={selectedSubcategory} onChange={onClick}>
-            <option value=""> Subcategories</option>
-            {subcategories.map((subcategory) => (
-              <option key={subcategory.name} value={subcategory.name}>
-                {subcategory.name}
-              </option>
-            ))}
-          </select>
-
-
-
-
-          <select value={selectedVideo} onChange={onClick}>
-            <option value=""> VideoList</option>
-            {videos.map((video) => (
-              <option key={video._id} value={video.title}>
-                {video.title}
-              </option>
-
-            ))}
-          </select>
-
-
-
-        </div>
-      )}
-
-    </div>
-
-  );
-
-}
-
-export default CategoryList;
-
-
- */
 
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -133,13 +10,12 @@ import Row from 'react-bootstrap/Row';
 
 function CategoryList() {
   const [categories, setCategories] = useState([]);
-  const [imageUrl, setImageUrl] = useState([]);
+  // const [imageUrl, setImageUrl] = useState([]);
 
   useEffect(() => {
     async function fetchCategories() {
       const response = await axios.get('http://localhost:5000/categories');
       setCategories(response.data);
-      setImageUrl(response.data);
     }
     fetchCategories();
   }, []);
@@ -149,7 +25,7 @@ function CategoryList() {
       {
         categories.map(category => (
           <Col>
-     <a class="thumbnail" href=""> 
+     <a class="thumbnail" href="/"> 
             <Card> 
               <Card.Img variant="top" style={{ height: '100px' }}
               
