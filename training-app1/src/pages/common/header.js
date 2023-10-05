f62db86dd6ae66bbba063698bf66ab83260da3e7
rajeshwari-lang <braji3806@gmail.com>
2023-10-05 11:32:51 +0530

    .

--- training-app1/src/pages/common/header.js
+++ training-app1/src/pages/common/header.js
@@ -22,7 +22,7 @@ export default function Header() {
                     <path fill="#fff" d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="0.2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                   </svg>
-                  <a href="/" className="text-base font-medium-3 font-bold text-black hover:text-black">Learn.</a>
+                  <a href="/" className="text-base font-medium font-bold text-black hover:text-black">Learn.</a>
                 </div>
 
               </a>
@@ -41,7 +41,6 @@ export default function Header() {
               <div className="hidden space-x-8 md:flex md:ml-10">
 
                 <a href={`category`} className="inline-flex text-base font-medium text-gray-500  hover:text-gray-300">Categories</a>
-                {/* <a href={`category`} className="inline-flex text-base font-medium text-gray-500  hover:text-gray-300">categories</a> */}
 
                 {/* <CategoryList/>                 */}
                 <a href="Home" className="text-base font-medium text-gray-500 hover:text-gray-300">Home</a>
