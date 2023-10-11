82ee3d0deca16b12a6049cb4181395dbc265f26d
Akash-Traveller <akash.arkvk@gmail.com>
2023-10-11 15:07:34 +0530

    changes - subscription , mychart, learning

--- training-app1/src/pages/common/Mylearn.js
+++ training-app1/src/pages/common/Mylearn.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card, Col, Row } from 'antd';
 
-function MyLearn() {
-  return (
-    <div>
-      <h1>My Learning page</h1>
-    </div>
-  );
-}
+const { Meta } = Card;
 
+const MyLearn = () => (
+
+  <Row
+    style={{
+      height: 500,
+      background: "LightGrey",
+      justifyContent: "center"
+    }}
+  >
+    <Col
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1
+      }}
+    >
+      <Card style={{ background: "gray" }} title="Card title">
+        <p>Card content, Card content, Card content</p>
+      </Card>
+    </Col>
+  </Row>
+
+);
 export default MyLearn;
