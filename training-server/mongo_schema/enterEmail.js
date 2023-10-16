c9853b09dc756645b41bd111c716ada22db2369f
Akash-Traveller <akash.arkvk@gmail.com>
2023-10-16 16:12:04 +0530

    changes - enteEmail feature

---
+++ training-server/mongo_schema/enterEmail.js
@@ -0,0 +1,28 @@
+const {model,Schema}=require("mongoose");
+const bcrypt=require("bcrypt")
+
+//signup user schema
+const userEnterSchema=new Schema({
+    email: {
+        type:String
+    }
+},{
+    collection:"enterEmail",
+});
+
+
+module.exports=model("enterEmail",userEnterSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
