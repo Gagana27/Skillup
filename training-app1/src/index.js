51eabb4972fff3cf75bc8bdd04b9091e905fd1be
DineshLogan <dineshlogan31@gmail.com>
2023-11-03 11:50:39 +0530

    Comments and BuyNow

--- training-app1/src/index.js
+++ training-app1/src/index.js
@@ -5,16 +5,20 @@ import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import {CartContextProvider} from "./context/CartContext"
 import { CommentContextProvider } from './context/CommentContext';
+import { SubscribedContextProvider } from './context/SubscribedContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
+    <CommentContextProvider>
     <CartContextProvider>
-   
+   <SubscribedContextProvider>
     <App />
-    
+    </SubscribedContextProvider>
     </CartContextProvider>
+    </CommentContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
